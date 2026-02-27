4cb77b40097db12d9eac3fd6738cbfb7eb1e079c
Jules-pecquereau <jules.pecquereau4@gmail.com>
2026-02-27 13:18:35 +0100

    bdd

---
+++ MedecineApp/BddRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Configuration; 
+
+
+
+namespace MedecineApp
+{
+    
+    public class BddRepository
+    {
+        private readonly string _connectionString;
+
+        public BddRepository(object cs)
+        {
+            _connectionString = ConfigurationManager.ConnectionStrings["BDDConnection"].ConnectionString;
+        }
+
+        public void Open()
+        {
+            var connection = new MySqlConnection(_connectionString);
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connection opened successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error opening connection: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ MedecineTest/BDDTest.cs
@@ -0,0 +1,21 @@
+﻿using MedecineApp;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedecineTest
+{
+    internal class BDDTest
+    {
+        [Test]
+        public void TestBDD()
+        {
+
+
+
+        }
+    }
+}
