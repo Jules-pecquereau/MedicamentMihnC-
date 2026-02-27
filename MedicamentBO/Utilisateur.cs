f7c4d18ea1deb146ce9e619012251d748e0ed985
Titouan-Lefort <102295079+Titouan-Lefort@users.noreply.github.com>
2026-02-27 12:15:13 +0100

    jk

--- MedicamentBO/Utilisateur.cs
+++ MedicamentBO/Utilisateur.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace MedicamentBO
 {
-    internal class Utilisateur
+    internal class Utilisateur : User
     {
+        public string MotDePasse { get; }
+        public string specialite { get; }
+        public bool IsAdmin { get; }
+        public bool IsActif { get; }
+
+        public Utilisateur(string nom, string prenom, string email, string motDePasse, string specialite, bool isAdmin, bool isActif): base(nom, prenom, email)
+        {
+            MotDePasse = motDePasse;
+            this.specialite = specialite;
+            IsAdmin = isAdmin;
+            IsActif = isActif;
+        }
+
+        public override string ToString()
+        {
+            return $"{Prenom} {Nom} - {Email} - {specialite} - {(IsAdmin ? "Admin" : "User")} - {(IsActif ? "Actif" : "Inactif")}";
+        }
     }
 }
