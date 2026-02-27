f9ae388870f53f6db3c15692ea9449f3969c3f07
Titouan-Lefort <102295079+Titouan-Lefort@users.noreply.github.com>
2026-02-27 11:43:00 +0100

    mise en place des return

--- MedicamentBO/Patient.cs
+++ MedicamentBO/Patient.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MedicamentBO
 {
-    internal class Patient
+    internal class Patient : User
     {
+        public string Adresse { get; }
+        public string CodePostal { get; }
+        public string Ville { get; }
+        public string Pays{ get; }
+        public int NumeroSecu { get; }
+        public int Telehone { get; }
+
+        public Patient(string nom, string prenom, string email, string adresse, string codePostal, string ville, string pays, int numeroSecu, int telehone) : base(nom, prenom, email)
+        {
+            Adresse = adresse;
+            CodePostal = codePostal;
+            Ville = ville;
+            Pays = pays;
+            NumeroSecu = numeroSecu;
+            Telehone = telehone;
+        }
+
+        public override string ToString()
+            {
+                return $"{Prenom} {Nom} - {Email} - {Adresse}, {CodePostal} {Ville}, {Pays} - N° Sécu: {NumeroSecu} - Téléphone: {Telehone}";
+        }
+
     }
 }
