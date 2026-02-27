fe23dd7f30fc7a2a84caf34a3e9c4a975856e109
Noan_Bregeon <furienocturne2.0@gmail.com>
2026-02-27 16:29:57 +0100

    modification

---
+++ MedicamentGUI/PatientPage.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace MedicamentGUI
+{
+    public partial class PatientPage : Page
+    {
+        public PatientPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
